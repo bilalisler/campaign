39b08d6afbefcaae560b4f1362bd6c7d4ab168dd
bilal <mercan_shark@windowslive.com>
2018-10-20 21:59:14 +0300

    base.html.twig was created as twig engine assets files was added

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends AbstractController
     public function index()
     {
 
-        return $this->render('home/index.html.twig', [
+        return $this->render('base/base.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
